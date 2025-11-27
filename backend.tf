9658911dc5155eb97b94fac01e2c2d8f5c44ba52
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-27 14:54:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatebackend001"
+    container_name       = "tfstate"
+    key                  = "cdn-profile/terraform.tfstate"
+  }
+}
