72ffbd8d07dd28a96a9036d7c3e5015bbb2de879
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 14:36:05 +0530

    Update main.tf

--- Modules/app_service_plan/main.tf
+++ Modules/app_service_plan/main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_app_service_plan" "asp" {
   name                = var.app_service_plan_name
-  location            = var.location
+  location            = var.app_service_plan_location
   resource_group_name = var.resource_group_name
 
   sku {
-    tier = var.tier
-    size = var.size
+    tier = var.app_service_plan_tier
+    size = var.app_service_plan_size
   }
 
-  os_type = var.os_type
+  os_type = var.app_service_plan_os_type
   tags    = var.app_service_plan_tags
 }
