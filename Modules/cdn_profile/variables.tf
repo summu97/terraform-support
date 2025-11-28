37f8eee9b14a496bc44f3273afd8edfba8b9af06
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 09:32:29 +0530

    Create variables.tf

---
+++ Modules/cdn_profile/variables.tf
@@ -0,0 +1,37 @@
+variable "cdn_profile_name" {
+  description = "The name of the CDN profile"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group where CDN profile will be created"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the CDN profile"
+  type        = string
+  default     = "East US"
+}
+
+variable "pricing_tier" {
+  description = <<EOF
+Available CDN SKU options:
+- Standard_Verizon
+- Premium_Verizon
+- Standard_Akamai
+- Standard_Microsoft  (Recommended)
+EOF
+  type = string
+}
+
+variable "tags" {
+  description = "Tags to assign to the CDN profile"
+  type        = map(string)
+  default     = {}
+}
