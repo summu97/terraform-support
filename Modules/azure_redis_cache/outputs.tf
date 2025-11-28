e80d278ca9a2aaf7e2b3a869cd6dc6790279f4f2
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 17:45:56 +0530

    Update outputs.tf

--- Modules/azure_redis_cache/outputs.tf
+++ Modules/azure_redis_cache/outputs.tf
@@ -1,11 +1,11 @@
 output "redis_id" {
-  value = azurerm_redis_cache.this.id
+  value = azurerm_redis_cache.one.id
 }
 
 output "redis_hostname" {
-  value = azurerm_redis_cache.this.hostname
+  value = azurerm_redis_cache.one.hostname
 }
 
 output "redis_port" {
-  value = azurerm_redis_cache.this.port
+  value = azurerm_redis_cache.one.port
 }
