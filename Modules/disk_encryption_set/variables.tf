69a9289980560bd12ac75386ac097f71028ec921
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 09:34:43 +0530

    Create variables.tf

---
+++ Modules/disk_encryption_set/variables.tf
@@ -0,0 +1,74 @@
+variable "key_vault_key_id" {
+  description = "The full resource id of the Key Vault key to be used by the Disk Encryption Set."
+  type        = string
+}
+
+variable "key_vault_resource_id" {
+  description = "The resource id of the Key Vault (or Managed HSM) that contains the key."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location to deploy the Disk Encryption Set into."
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the Disk Encryption Set resource."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name where the Disk Encryption Set will be created."
+  type        = string
+}
+
+variable "auto_key_rotation_enabled" {
+  description = "Enable automatic key rotation for the Disk Encryption Set."
+  type        = bool
+  default     = false
+}
+
+variable "enable_telemetry" {
+  description = "Enable telemetry/usage reporting via modtm provider."
+  type        = bool
+  default     = true
+}
+
+variable "encryption_type" {
+  description = "Type of encryption for the Disk Encryption Set."
+  type        = string
+  default     = "EncryptionAtRestWithCustomerKey"
+}
+
+variable "federated_client_id" {
+  description = "(Optional) Principal id (managed identity or service principal) that should be granted access to the key."
+  type        = string
+  default     = null
+}
+
+variable "managed_hsm_key_id" {
+  description = "(Optional) Managed HSM key id, if using Managed HSM rather than Key Vault."
+  type        = string
+  default     = null
+}
+
+variable "lock" {
+  description = <<EOF
+Optional management lock object. Set to null to skip creating a lock.
+If provided it must contain:
+  - kind = "CanNotDelete" | "ReadOnly"
+  - name = (optional) lock name. If omitted a default name will be used.
+EOF
+  type = object({
+    kind = string
+    name = optional(string, null)
+  })
+  default = null
+}
+
+variable "tags" {
+  description = "Optional tags to apply to resources."
+  type        = map(string)
+  default     = {}
+}
