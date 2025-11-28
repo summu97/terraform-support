65472ed3c1d35054ca304ee60d81fdd91fff0f42
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 16:51:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -125,55 +125,79 @@ variable "cdn_tags" {
 # DISK ENCRYPTION SET (DES) VARIABLES
 ############################
 
+variable "des_key_vault_key_id" {
+  description = "The full resource id of the Key Vault key to be used by the Disk Encryption Set."
+  type        = string
+}
+
+variable "des_key_vault_resource_id" {
+  description = "The resource id of the Key Vault (or Managed HSM) that contains the key."
+  type        = string
+}
+
+variable "des_location" {
+  description = "Azure location to deploy the Disk Encryption Set into."
+  type        = string
+}
+
 variable "des_name" {
-  description = "Disk Encryption Set name"
+  description = "Name of the Disk Encryption Set resource."
   type        = string
 }
 
-variable "key_vault_key_id" {
-  description = "Key Vault key ID"
+variable "resource_group_name" {
+  description = "Resource group name where the Disk Encryption Set will be created."
   type        = string
 }
 
-variable "key_vault_resource_id" {
-  description = "Key Vault/Managed HSM resource ID"
-  type        = string
-}
-
-variable "auto_key_rotation_enabled" {
+variable "des_auto_key_rotation_enabled" {
+  description = "Enable automatic key rotation for the Disk Encryption Set."
   type        = bool
   default     = false
 }
 
 variable "des_enable_telemetry" {
+  description = "Enable telemetry/usage reporting via modtm provider."
   type        = bool
   default     = true
 }
 
 variable "des_encryption_type" {
-  description = "Encryption type"
+  description = "Type of encryption for the Disk Encryption Set."
   type        = string
   default     = "EncryptionAtRestWithCustomerKey"
 }
 
 variable "des_federated_client_id" {
-  description = "Principal ID for access"
+  description = "(Optional) Principal id (managed identity or service principal) that should be granted access to the key."
   type        = string
   default     = null
 }
 
 variable "des_managed_hsm_key_id" {
+  description = "(Optional) Managed HSM key id, if using Managed HSM rather than Key Vault."
   type        = string
   default     = null
 }
 
 variable "des_lock" {
-  description = "Optional lock configuration"
+  description = <<EOF
+Optional management lock object. Set to null to skip creating a lock.
+If provided it must contain:
+  - kind = "CanNotDelete" | "ReadOnly"
+  - name = (optional) lock name. If omitted a default name will be used.
+EOF
   type = object({
     kind = string
     name = optional(string, null)
   })
   default = null
+}
+
+variable "des_tags" {
+  description = "Optional tags to apply to resources."
+  type        = map(string)
+  default     = {}
 }
 
 
