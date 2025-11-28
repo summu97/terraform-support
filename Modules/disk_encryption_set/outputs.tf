f147dea2344074d973f43cbfd8d635da41781b3a
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 09:34:12 +0530

    Create outputs.tf

---
+++ Modules/disk_encryption_set/outputs.tf
@@ -0,0 +1,14 @@
+output "key_vault_key_url" {
+  description = "The URL of the key in Key Vault / Managed HSM."
+  value       = var.managed_hsm_key_id != null ? var.managed_hsm_key_id : var.key_vault_key_id
+}
+
+output "resource" {
+  description = "The full Disk Encryption Set resource object."
+  value       = azurerm_disk_encryption_set.this
+}
+
+output "resource_id" {
+  description = "The resource id of the Disk Encryption Set."
+  value       = azurerm_disk_encryption_set.this.id
+}
