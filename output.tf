abce8e13221d56037520cb7ce021393bdd1e8c29
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 10:00:41 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,56 @@
+# -------------------------
+# Redis Module Outputs
+# -------------------------
+output "redis_id" {
+  value = module.redis.redis_id
+}
+
+output "redis_hostname" {
+  value = module.redis.redis_hostname
+}
+
+output "redis_port" {
+  value = module.redis.redis_port
+}
+
+# -------------------------
+# CDN Module Outputs
+# -------------------------
+output "cdn_profile_id" {
+  value = module.cdn.cdn_profile_id
+}
+
+output "cdn_profile_name" {
+  value = module.cdn.cdn_profile_name
+}
+
+# -------------------------
+# Disk Encryption Set Outputs
+# -------------------------
+output "des_key_vault_key_url" {
+  value = module.des.key_vault_key_url
+}
+
+output "des_resource" {
+  value = module.des.resource
+}
+
+output "des_resource_id" {
+  value = module.des.resource_id
+}
+
+# -------------------------
+# Autoscale Module Outputs
+# -------------------------
+output "autoscale_resource_id" {
+  value = module.autoscale.resource_id
+}
+
+output "autoscale_resource_name" {
+  value = module.autoscale.resource_name
+}
+
+output "autoscale_resource" {
+  value = module.autoscale.resource
+}
 
