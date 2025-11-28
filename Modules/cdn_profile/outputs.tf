d39765bfa262c59027457c314715b4db2af2bed8
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 17:47:46 +0530

    Update outputs.tf

--- Modules/cdn_profile/outputs.tf
+++ Modules/cdn_profile/outputs.tf
@@ -1,9 +1,9 @@
 output "cdn_profile_id" {
   description = "ID of the CDN profile"
-  value       = azurerm_cdn_profile.this.id
+  value       = azurerm_cdn_profile.one.id
 }
 
 output "cdn_profile_name" {
   description = "Name of the CDN profile"
-  value       = azurerm_cdn_profile.this.name
+  value       = azurerm_cdn_profile.one.name
 }
