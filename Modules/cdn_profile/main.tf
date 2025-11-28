85051b96fddc146f4b100f118ea308ff6a257c7f
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 17:48:03 +0530

    Update main.tf

--- Modules/cdn_profile/main.tf
+++ Modules/cdn_profile/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_cdn_profile" "this" {
+resource "azurerm_cdn_profile" "one" {
   name                = var.cdn_profile_name
   resource_group_name = var.resource_group_name
   location            = var.cdn_location
