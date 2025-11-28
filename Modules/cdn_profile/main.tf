174ef9d0594e5e20923cee3f05301b6be37d4412
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 09:31:42 +0530

    Create main.tf

---
+++ Modules/cdn_profile/main.tf
@@ -0,0 +1,12 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_cdn_profile" "this" {
+  name                = var.cdn_profile_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.pricing_tier
+
+  tags = var.tags
+}
