c65df52a6b8bee725dc461457f97bfafb35ba636
summu97 <135579543+summu97@users.noreply.github.com>
2025-11-28 17:56:14 +0530

    Update outputs.tf

--- Modules/disk_encryption_set/outputs.tf
+++ Modules/disk_encryption_set/outputs.tf
@@ -1,6 +1,6 @@
 output "key_vault_key_url" {
   description = "The URL of the key in Key Vault / Managed HSM."
-  value       = var.managed_hsm_key_id != null ? var.managed_hsm_key_id : var.key_vault_key_id
+  value       = var.des_managed_hsm_key_id != null ? var.des_managed_hsm_key_id : var.des_key_vault_key_id
 }
 
 output "resource" {
