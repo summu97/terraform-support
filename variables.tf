2d5ffc558a5ad3adbf8b50aea0363b6e576d92ac
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-01 19:41:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -317,34 +317,41 @@ variable "autoscale_tags" {
 #---------------------------
 # app_service_plan
 #---------------------------
-variable "app_service_plan_location" {
-  description = "app_service_plan Region for resources"
-  type        = string
-}
-
 variable "app_service_plan_name" {
   type        = string
-  description = "Name of the App Service Plan"
+  description = "Name of the App Service Plan."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "app_service_plan_location" {
+  type        = string
+  description = "Azure region for the App Service Plan."
 }
 
 variable "app_service_plan_tier" {
   type        = string
-  description = "Pricing tier (Basic, Standard, Premium)"
+  description = "Pricing tier for the App Service Plan (e.g., Standard, Premium)."
+  default     = "Standard"
 }
 
 variable "app_service_plan_size" {
   type        = string
-  description = "Instance size (B1, S1, P1v3)"
+  description = "SKU name for the App Service Plan (e.g., S1, P1v2)."
+  default     = "S1"
 }
 
-variable "app_service_plan_os_type" {
+variable "app_service_plan_os" {
   type        = string
-  description = "OS type to run the plan"
+  description = "Operating system for the App Service Plan ('Windows' or 'Linux')."
   default     = "Linux"
 }
 
 variable "app_service_plan_tags" {
   type        = map(string)
+  description = "Tags for the App Service Plan."
   default     = {}
 }
-
