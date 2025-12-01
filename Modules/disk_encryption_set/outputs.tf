c29ee82919eaf3d1993f890184ad7348df39ea29
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-01 17:20:03 +0530

    Update outputs.tf

--- Modules/disk_encryption_set/outputs.tf
+++ Modules/disk_encryption_set/outputs.tf
@@ -1,8 +1,3 @@
-output "resource" {
-  description = "The full Disk Encryption Set resource object."
-  value       = azurerm_disk_encryption_set.one
-}
-
 output "resource_id" {
   description = "The resource id of the Disk Encryption Set."
   value       = azurerm_disk_encryption_set.one.id
