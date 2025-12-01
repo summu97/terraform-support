7cbb41ea89383ecbc104018ad67e0585d0565ca7
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-01 17:25:44 +0530

    Update variables.tf

--- Modules/app_service_plan/variables.tf
+++ Modules/app_service_plan/variables.tf
@@ -1,29 +1,36 @@
 variable "app_service_plan_name" {
-  type = string
+  type        = string
+  description = "Name of the App Service Plan."
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the resource group."
 }
 
 variable "app_service_plan_location" {
-  type = string
+  type        = string
+  description = "Azure region for the App Service Plan."
 }
 
 variable "app_service_plan_tier" {
-  type = string
+  type        = string
+  description = "Pricing tier for the App Service Plan (e.g., Standard, Premium)."
 }
 
 variable "app_service_plan_size" {
-  type = string
+  type        = string
+  description = "Size for the App Service Plan (e.g., S1, P1v2)."
 }
 
-variable "app_service_plan_os_type" {
-  type    = string
-  default = "Linux"
+variable "app_service_plan_os" {
+  type        = string
+  description = "Operating system for the App Service Plan ('Windows' or 'Linux')."
+  default     = "Linux"
 }
 
 variable "app_service_plan_tags" {
-  type    = map(string)
-  default = {}
+  type        = map(string)
+  description = "Tags for the App Service Plan."
+  default     = {}
 }
