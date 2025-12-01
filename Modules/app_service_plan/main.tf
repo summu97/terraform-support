b9cd8d563d2d69ebda0bd609ffd97bb5a7ac15f0
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-01 17:25:26 +0530

    Update main.tf

--- Modules/app_service_plan/main.tf
+++ Modules/app_service_plan/main.tf
@@ -3,11 +3,14 @@ resource "azurerm_app_service_plan" "asp" {
   location            = var.app_service_plan_location
   resource_group_name = var.resource_group_name
 
+  # Determine kind & reserved based on OS
+  kind     = var.app_service_plan_os == "Linux" ? "Linux" : "App"
+  reserved = var.app_service_plan_os == "Linux" ? true : false
+
   sku {
     tier = var.app_service_plan_tier
     size = var.app_service_plan_size
   }
 
-  os_type = var.app_service_plan_os_type
-  tags    = var.app_service_plan_tags
+  tags = var.app_service_plan_tags
 }
