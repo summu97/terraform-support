8ff4ea4568c1a30c7f02958a559449db15d18381
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-02 12:50:53 +0530

    Update outputs.tf

--- Modules/azure_frontdoor/outputs.tf
+++ Modules/azure_frontdoor/outputs.tf
@@ -1,9 +1,9 @@
 output "profile_id" {
   description = "Front Door profile resource id"
-  value       = azurerm_cdn_frontdoor_profile.this.id
+  value       = azurerm_frontdoor_profile.this.id
 }
 
 output "endpoint_hostname" {
   description = "Front Door endpoint host name"
-  value = [for fe in azurerm_cdn_frontdoor_endpoint.this : fe.value.host_name]
+  value = [for fe in azurerm_frontdoor_endpoint.this : fe.value.host_name]
 }
