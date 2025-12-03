0e0af80bf319c2ee1fefbfc3d3e7ee8f5cb98cf5
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-03 10:23:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -345,3 +345,133 @@ variable "app_service_plan_tags" {
   default     = {}
 }
 
+#---------------------------
+# azure_cdn_frontdoor
+#---------------------------
+
+variable "frontdoor_location" {
+  type        = string
+  description = "Azure location"
+  default     = "East US"
+}
+
+variable "frontdoor_custom_name" {
+  type        = string
+  description = "Custom name for the Front Door"
+  default     = ""
+}
+
+variable "frontdoor_name_prefix" { type = string }
+variable "frontdoor_client_name" { type = string }
+variable "frontdoor_environment" { type = string }
+variable "frontdoor_stack" { type = string }
+variable "frontdoor_name_suffix" { type = string }
+
+variable "frontdoor_sku_name" {
+  type        = string
+  description = "Front Door SKU (Standard_AzureFrontDoor or Premium_AzureFrontDoor)"
+  default     = "Premium_AzureFrontDoor"
+}
+
+variable "frontdoor_extra_tags" {
+  type        = map(string)
+  description = "Tags to attach"
+  default     = {}
+}
+
+variable "frontdoor_identity" {
+  type = map(any)
+  default = {
+    type = "SystemAssigned"
+  }
+}
+
+variable "frontdoor_custom_domains" {
+  type = list(object({
+    name      = string
+    host_name = string
+    tls = optional(object({
+      certificate_type         = optional(string, "ManagedCertificate")
+      minimum_tls_version      = optional(string, "TLS12")
+      cdn_frontdoor_secret_id  = optional(string, null)
+    }), {})
+  }))
+  default = []
+
+  validation {
+    condition = alltrue([
+      for d in var.custom_domains :
+      (
+        d.tls == {} ||
+        lookup(d.tls, "minimum_tls_version", "TLS12") != "TLS10"
+      )
+    ])
+
+    error_message = "TLS10 is deprecated for Azure Front Door. Use TLS12."
+  }
+}
+
+variable "frontdoor_endpoints" {
+  type = list(object({
+    name    = string
+    enabled = optional(bool, true)
+  }))
+  default = []
+}
+
+variable "frontdoor_origin_groups" {
+  type = list(object({
+    name                                        = string
+    session_affinity_enabled                    = optional(bool, true)
+    restore_traffic_time_to_healed_or_new_endpoint_in_minutes = optional(number, 10)
+    health_probe = optional(object({
+      interval_in_seconds = number
+      path                = string
+      protocol            = string
+      request_type        = string
+    }))
+    load_balancing = optional(object({
+      additional_latency_in_milliseconds = number
+      sample_size                        = number
+      successful_samples_required        = number
+    }))
+    origins = list(object({
+      host_name   = string
+      http_port   = optional(number, 80)
+      https_port  = optional(number, 443)
+      priority    = optional(number, 1)
+      weight      = optional(number, 1)
+    }))
+  }))
+  default = []
+}
+
+variable "frontdoor_origins" {
+  type = list(object({
+    name                          = string
+    origin_group_name             = string
+    host_name                     = string
+    http_port                      = optional(number, 80)
+    https_port                     = optional(number, 443)
+    priority                       = optional(number, 1)
+    weight                         = optional(number, 1)
+    enabled                        = optional(bool, true)
+    certificate_name_check_enabled = optional(bool, true)
+  }))
+  default = []
+}
+
+variable "frontdoor_routes" {
+  type = list(object({
+    name                   = string
+    endpoint_name          = string
+    origin_names          = list(string)
+    origin_group_name      = string
+    forwarding_protocol    = optional(string, "HttpsOnly")
+    https_redirect_enabled = optional(bool, true)
+    patterns_to_match      = optional(list(string), ["/*"])
+    supported_protocols    = optional(list(string), ["Https"])
+  }))
+  default = []
+}
+
