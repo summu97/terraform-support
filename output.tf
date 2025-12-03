69001ffbcc7c29ded9db3de3efed57cc7db77b93
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-03 10:42:35 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,38 @@
 # -------------------------
+# App Service Plan
+# -------------------------
+output "app_service_plan_id" {
+  value = module.app_service_plan.id
+}
+
+# -------------------------
+# Azure CDN Frontdoor
+# -------------------------
+output "frontdoor_frontdoor_profile" {
+  value = module.azure_cdn_frontdoor.frontdoor_profile
+}
+
+output "frontdoor_custom_domains" {
+  value = module.azure_cdn_frontdoor.
+}
+
+output "frontdoor_endpoints" {
+  value = module.azure_cdn_frontdoor.custom_domains
+}
+
+output "frontdoor_origin_groups" {
+  value = module.azure_cdn_frontdoor.origin_groups
+}
+
+output "frontdoor_origins" {
+  value = module.azure_cdn_frontdoor.origins
+}
+
+output "frontdoor_routes" {
+  value = module.azure_cdn_frontdoor.routes
+}
+
+# -------------------------
 # Redis Module Outputs
 # -------------------------
 output "redis_id" {
@@ -11,17 +45,6 @@ output "redis_hostname" {
 
 output "redis_port" {
   value = module.azure_redis_cache.redis_port
-}
-
-# -------------------------
-# CDN Module Outputs
-# -------------------------
-output "cdn_profile_id" {
-  value = module.cdn_profile.cdn_profile_id
-}
-
-output "cdn_profile_name" {
-  value = module.cdn_profile.cdn_profile_name
 }
 
 # -------------------------
