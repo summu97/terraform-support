4dbb9687b92b54b840b95eae55358dfd3ecc67bc
summu97 <135579543+summu97@users.noreply.github.com>
2025-12-03 10:33:41 +0530

    Delete Modules/cdn_profile directory

--- Modules/cdn_profile/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_cdn_profile" "one" {
-  name                = var.cdn_profile_name
-  resource_group_name = var.resource_group_name
-  location            = var.cdn_location
-  sku                 = var.cdn_pricing_tier
-
-  tags = var.cdn_tags
-}
--- Modules/cdn_profile/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cdn_profile_id" {
-  description = "ID of the CDN profile"
-  value       = azurerm_cdn_profile.one.id
-}
-
-output "cdn_profile_name" {
-  description = "Name of the CDN profile"
-  value       = azurerm_cdn_profile.one.name
-}
